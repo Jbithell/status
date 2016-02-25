0659ca3137955ee3218d87554cdf2472a6b2c935
James Bithell <Jbithell@users.noreply.github.com>
2016-02-25 20:17:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ var monitors = [
     
 ];
 var title = 'My Project Status Page'; //Page <title> and heading at top of page
-var refreshtime = 5; //How often to refresh in seconds
+var refreshtime = 60; //How often to refresh in seconds
 var footermessage = ''; //HTML to go in footer
